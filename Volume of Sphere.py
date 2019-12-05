410956b3906f1c4f2e8d93601d2fb94c05d5733c
BasimAhmedKhan <50414240+BasimAhmedKhan@users.noreply.github.com>
2019-12-06 00:05:24 +0500

    Add files via upload

---
+++ Volume of Sphere.py
@@ -0,0 +1,8 @@
+print(" ")
+print('"VOLUME OF SPHERE!"')
+print(" ")
+
+rad  = float(input('"Enter Radius OF Sphere": '))
+volume = ((4 / 3) * 3.1415926535897932384626433832795 * rad**3)
+
+print("Volume Of Sphere is: " + str(volume))
